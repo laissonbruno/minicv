9436d9534e650d4c9d373ea6dc72fda5b7d81d41
Laisson Bruno <laissontkd@hotmail.com>
2023-09-27 19:37:27 -0300

    minicv

---
+++ assets/script/acordeon.js
@@ -0,0 +1,15 @@
+const triggers = document.querySelectorAll('.acordeon .trigger')
+
+triggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+
+        const isOpen = acordeon.classList.contains('open')
+
+        if (isOpen) {
+            acordeon.classList.remove('open')
+        } else {
+            acordeon.classList.add('open')
+        }
+    })
+})
